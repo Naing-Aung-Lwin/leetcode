28deccc0c8c5f360621e13503e936ffb48ca3a57
Naing Aung Lwin <naingaunglwin@yomabank.com>
2025-02-21 16:47:41 +0630

    no.8 - in progress

---
+++ 8.StringToInteger(atoi)/index.js
@@ -0,0 +1,40 @@
+/**
+ * https://leetcode.com/problems/string-to-integer-atoi
+ */
+var myAtoi = function(s) {
+    const arr = s.split('');
+    let result = '';
+    for (let i = 0; i < arr.length; i++) {
+        let a = arr[i];
+        if (isFirstSpace(a, result)) {
+            continue;
+        }
+        if (isNumber(a) || isFirstNegative(a, result)) {
+            result += a;
+            continue
+        }
+        break;
+    }
+    const min = Math.pow(-2, 31);
+    const max = Math.pow(2, 31) - 1;
+    let num = Number(result.trim());
+    if (num < min) return min;
+    if (num > max) return max;
+    return num;
+};
+
+var isNumber = function (str) {
+    return /^\d+$/.test(str);
+}
+
+var isFirstNegative = function (str, result) {
+    return str === '-' && result.length === 0;
+}
+
+var isFirstSpace = function (str, result) {
+    return str === ' ' && result.length === 0;
+}
+
+
+const s = "-+12";
+console.log(myAtoi(s));
